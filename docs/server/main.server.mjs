8a27ed15101f61deee5d4296c8f80d29f0f45fb0
unanimous <200100518@luanar.ac.mw>
2024-07-08 14:58:50 +0200

    first second

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZM6RBRZH.mjs";import"./chunk-565QRG4E.mjs";import"./chunk-4C2SD57Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-4CXMF7FV.mjs";import"./chunk-565QRG4E.mjs";import"./chunk-4C2SD57Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
